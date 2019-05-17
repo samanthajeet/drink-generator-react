62d57c638a870dd2c38f0bfab97cc1b7b8b46e16
Samantha Jeet <samanthajeet@gmail.com>
2019-05-17 15:25:26 -0700

    initial set up

---
+++ server/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+
+const app = express();
+const { SERVER_PORT, CONNECTION_STRING } = process.env
+
+app.use(express.json())
+
+massive(CONNECTION_STRING).then( db => {
+    app.set('db', db);
+    app.listen( SERVER_PORT, () => console.log(`bingpot on ${SERVER_PORT}`))
+})
---
+++ src/components/Cabinet/Cabinet.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Cabinet = () => {
+  useEffect(() => {});
+
+  return (
+    <>
+      <h1>your cabinet!</h1>
+
+      <input />
+      <label>
+        <input type="radio" value="hard" />
+        hard alcohol
+      </label>
+    </>
+  );
+};
+
+export default Cabinet;
